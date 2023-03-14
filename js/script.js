180cacfddd14628b9059ff8e2b3c6b6b1c34232c
chloew3st <chloew3st@gmail.com>
2023-03-14 16:36:21 -0500

    updated button to react to clicks

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@ var cat = document.querySelector(".cat");
 //console.log(cat);
 
 button.addEventListener("click", function() {
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        //console.log("yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        //console.log("no");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    }
 });
